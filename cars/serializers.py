e597b53d476687496cf7bcef3d8d0c43f19ea578
yellowmint <michnajacek0@gmail.com>
2020-10-20 23:47:37 +0200

    Add custom car serializer validation

--- cars/serializers.py
+++ cars/serializers.py
@@ -3,7 +3,11 @@ from rest_framework import serializers
 from cars.models import Car
 
 
-class CarSerializer(serializers.HyperlinkedModelSerializer):
+class CarSerializer(serializers.ModelSerializer):
     class Meta:
         model = Car
         fields = ['id', 'make_name', 'model_name']
+
+    def validate(self, attributes):
+        if attributes['make_name'] == attributes['model_name']:
+            raise serializers.ValidationError('make and model names cannot be equal')
--- cars/views.py
+++ cars/views.py
@@ -7,9 +7,3 @@ from cars.serializers import CarSerializer
 class CarViewSet(mixins.CreateModelMixin, mixins.ListModelMixin, viewsets.GenericViewSet):
     queryset = Car.objects.all()
     serializer_class = CarSerializer
-
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
-
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
