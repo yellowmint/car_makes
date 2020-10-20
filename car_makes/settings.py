02c4d9060d5a695c5d1e1b7954c50c3d59d17ac7
yellowmint <michnajacek0@gmail.com>
2020-10-20 20:40:00 +0200

    Add car model

--- car_makes/settings.py
+++ car_makes/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'car-makes.herokuapp.com']
 # Application definition
 
 INSTALLED_APPS = [
+    'cars.apps.CarsConfig',
     'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
--- cars/models.py
+++ cars/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+
+class Car(models.Model):
+    make_name = models.CharField(max_length=250)
+    model_name = models.CharField(max_length=250)
