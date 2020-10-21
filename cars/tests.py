ecd405aea77e8c2952ec3744b2fd0cc488a117a5
yellowmint <michnajacek0@gmail.com>
2020-10-21 13:42:51 +0200

    Add car creation test (#4)

--- cars/tests.py
+++ cars/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from rest_framework.reverse import reverse
 
-# Create your tests here.
+
+class CarViewTests(TestCase):
+    def test_create_success(self):
+        resp = self.client.post(reverse('car-list'), {'make_name': 'fiat', 'model_name': 'brava'})
+        self.assertEqual(resp.status_code, 201)
+
+    def test_create_invalid_params(self):
+        resp = self.client.post(reverse('car-list'), {'wrong': 'foo', 'data': 'bar'})
+        self.assertEqual(resp.status_code, 400)
+
+        want = {'make_name': ['This field is required.'], 'model_name': ['This field is required.']}
+        self.assertEqual(resp.json(), want)
